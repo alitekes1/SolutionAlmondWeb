aab03e5a3cbf3808aa90f215185b7d7d7faf7d6f
Ali Tekeş <ali.tekes@ogr.sakarya.edu.tr>
2023-09-07 13:24:28 +0300

    login ve registerdaki backgroundlar güncellendi.

--- AlmondWeb/Scripts/AlmondJs/index.js
+++ AlmondWeb/Scripts/AlmondJs/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var Images = ["/Images/background/bg1.jpg", "/Images/background/bg2.jpg", "/Images/background/bg9.jpg", "/Images/background/bg4.jpg", "/Images/background/bg5.jpg", "/Images/background/bg6.jpg", "/Images/background/bg7.jpg", "/Images/background/bg8.jpg", "/Images/background/bg10.jpg", "/Images/background/bg11.jpg", "/Images/background/bg12.jpg", "/Images/background/bg13.jpg"];
+    var Images = ["/Images/background/bg1.jpg", "/Images/background/bg6.jpg", "/Images/background/bg9.jpg", "/Images/background/bg4.jpg", "/Images/background/bg5.jpg", "/Images/background/bg6.jpg", "/Images/background/bg7.jpg", "/Images/background/bg6.jpg", "/Images/background/bg6.jpg", "/Images/background/bg11.jpg", "/Images/background/bg12.jpg", "/Images/background/bg13.jpg"];
     var currentIndex = Math.floor(Math.random() * Images.length);
     var activeSlide = document.body;
     activeSlide.style.backgroundImage = "url('" + Images[currentIndex] + "')";
